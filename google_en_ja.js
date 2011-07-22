288f50916d7bde8f29223773c18c1eb7c9e706cf
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2011-07-22 11:56:53 +0900

    Initial google_en_ja.js

---
+++ google_en_ja.js
@@ -0,0 +1,10 @@
+var r=RegExp,l=location;
+if(l.href.match(new r('^(http://[^/]+\\.google\\.[^/]+\/search)(\\?[^#]+)(#.*\\bq=.*)?'))){
+    var u=r.$1,p=r.$3||r.$2,q=(p.match(/[&#\?]q=([^&]*)/))[1];
+    if(q){
+        if(p.match(/[&#\?]lr=([^&]*)/))var lr=r.$1;
+        if(p.match(/[&#\?]hl=([^&]*)/))var hl=r.$1;
+        var nhl=(lr=='lang_ja'?'en':hl=='ja'?'en':'ja');
+        l.href=u+'?q='+q+'&hl='+nhl+'&lr='+{'en':'','ja':'lang_ja'}[nhl];
+    }
+}
